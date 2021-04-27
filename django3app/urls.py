d3be1b37abbc4694735974991f2b6f4c4c8dd63b
Fauzi Ihsan <fauzyihsan@gmail.com>
2021-04-27 10:06:28 +0700

    Video 7

--- django3app/urls.py
+++ django3app/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views as blogViews
-from about import views as aboutViews
+from django.urls import path, include
+
 
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('', views.index),
-    path('blog', blogViews.index),
-    path('about/', aboutViews.index),
+    path('about/', include('about.urls')),
 ]
