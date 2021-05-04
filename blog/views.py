224111620cd06e7a936847b74ae6228b2fd20f39
Fauzi Ihsan <fauzyihsan@gmail.com>
2021-05-04 17:03:43 +0700

    Video 18

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Post
+
 def index(request):
+    #  Queryset
+    posts = Post.objects.all()
     context = {
         'judul': 'Blog',
         'kontributor': 'Parent dari Blog',
@@ -13,9 +17,10 @@ def index(request):
             ['/contact','Contact'],
         ],
         'banner': 'blog/img/banner_blog.png',
-        'app_css': 'blog/css/styles.css'
+        'app_css': 'blog/css/styles.css',
+        'posts': posts
     }
-    return render(request, 'index.html', context)
+    return render(request, 'blog/index.html', context)
 
 def cerita(request):
     context = {
